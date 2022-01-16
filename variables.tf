007e16308319f1bc244e6c35aa08d9d5ed47b049
kanade <melty0404@gmail.com>
2022-01-16 22:11:07 +0900

    feat: enable firestore

--- variables.tf
+++ variables.tf
@@ -4,26 +4,49 @@ variable "PROJECT_ID" {
 variable "GOOGLE_CREDENTIALS" {}
 locals {
   services = [
+    // BigQuery API
     "bigquery.googleapis.com",
+    // BigQuery Storage API
     "bigquerystorage.googleapis.com",
+    // Cloud Datastore API
     "datastore.googleapis.com",
+    // Cloud Debugger API
     "clouddebugger.googleapis.com",
+    // Cloud Functions API
     "cloudfunctions.googleapis.com",
+    // Cloud Logging API
     "logging.googleapis.com",
+    // Cloud Monitoring API
     "monitoring.googleapis.com",
+    // Cloud Pub/Sub API
     "pubsub.googleapis.com",
+    // Cloud SQL
     "sql-component.googleapis.com",
+    // Cloud Storage
     "storage-component.googleapis.com",
+    // Cloud Storage API
+    "storage.googleapis.com",
+    // Cloud Trace API
     "cloudtrace.googleapis.com",
+    // Google Cloud APIs
     "cloudapis.googleapis.com",
+    // Google Cloud Storage JSON API
     "storage-api.googleapis.com",
+    // Service Management API
     "servicemanagement.googleapis.com",
+    // Service Usage API
     "serviceusage.googleapis.com",
+    // Cloud Resource Manager API
     "cloudresourcemanager.googleapis.com",
-    "cloudfunctions.googleapis.com",
+    // Cloud Build API
     "cloudbuild.googleapis.com",
+    // Cloud Key Management Service (KMS) API	
     "cloudkms.googleapis.com",
+    // Cloud Firestore API
     "firestore.googleapis.com",
-    "appengine.googleapis.com"
+    // App Engine Admin API
+    "appengine.googleapis.com",
+    // Firebase Rules API
+    "firebaserules.googleapis.com"
   ]
 }
