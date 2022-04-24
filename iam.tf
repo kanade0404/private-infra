a09180a38497d2e7f3b28ee25104a0ae0c9c72e9
kanade <melty0404@gmail.com>
2022-04-24 20:35:26 +0900

    :sparkles: add repositories UR賃貸スクレイピングリポジトリ Zennスクレイピングリポジトリ

--- iam.tf
+++ iam.tf
@@ -1,9 +1,14 @@
-variable "repo-name" {
-  type    = string
-  default = "kanade0404/private-infra"
+locals {
+  username = "kanade0404"
+  repositories = [
+    "${local.username}/private-infra",
+    "${local.username}/ur",
+    "${local.username}/scraping-zenn",
+  ]
 }
 resource "google_service_account_iam_member" "admin-account-iam" {
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-actions.name}/attribute.repository/${var.repo-name}"
+  for_each           = toset(local.repositories)
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-actions.name}/attribute.repository/${each.value}"
   role               = "roles/iam.workloadIdentityUser"
   service_account_id = google_service_account.github-actions.name
 }
