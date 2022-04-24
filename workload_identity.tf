d1db66d8efdc77ada3673df12e996ceb965dbc6c
kanade <melty0404@gmail.com>
2022-04-24 17:22:14 +0900

    :bug: Add attribute mapping

--- workload_identity.tf
+++ workload_identity.tf
@@ -16,6 +16,8 @@ resource "google_iam_workload_identity_pool_provider" "github-actions" {
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
     "attribute.repository" = "assertion.repository"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
   }
   oidc {
     issuer_uri        = "https://token.actions.githubusercontent.com"
