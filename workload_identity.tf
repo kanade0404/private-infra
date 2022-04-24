f915d1e5621a957f3b67172cb3017d169fcad951
kanade <melty0404@gmail.com>
2022-04-24 17:23:53 +0900

    :bug: Remove allowed_audience optionalなので、追加すると一切の`aud`fieldが拒否される

--- workload_identity.tf
+++ workload_identity.tf
@@ -23,7 +23,6 @@ resource "google_iam_workload_identity_pool_provider" "github-actions" {
     "attribute.aud"        = "assertion.aud"
   }
   oidc {
-    issuer_uri        = "https://token.actions.githubusercontent.com"
-    allowed_audiences = []
+    issuer_uri = "https://token.actions.githubusercontent.com"
   }
 }
