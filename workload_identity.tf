c8d9b3d8e59f9a2531c9d1b8a0860806a97c0145
kanade <melty0404@gmail.com>
2022-04-25 09:59:56 +0900

    :bug: Remove attribute.actor

--- workload_identity.tf
+++ workload_identity.tf
@@ -19,7 +19,6 @@ resource "google_iam_workload_identity_pool_provider" "github-actions" {
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
     "attribute.repository" = "assertion.repository"
-    "attribute.actor"      = "assertion.actor"
   }
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
